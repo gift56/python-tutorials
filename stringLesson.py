c933ca821c683e981c42c59ac2a22e3998f6a3e8
Efe G <98314362+gift56@users.noreply.github.com>
2025-04-18 17:28:49 -0700

    learning python syntax

---
+++ stringLesson.py
@@ -0,0 +1,14 @@
+print("It's alright")
+
+# Setting variable in multiple lines
+
+a = """Lorem ipsum dolor sit amet,
+consectetur adipiscing elit,
+sed do eiusmod tempor incididunt
+ut labore et dolore magna aliqua."""
+print(a)
+
+# Looping through string characters
+
+for x in "banana":
+  print(x)
