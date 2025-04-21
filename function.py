b1b9a1d98e37eb8d5c473b2f8403b3947ef2f0e8
Efe G <98314362+gift56@users.noreply.github.com>
2025-04-21 15:09:29 -0700

    working on some update

--- function.py
+++ function.py
@@ -2,3 +2,10 @@ def my_function():
     print("Hello, World!")
 
 my_function()
+
+# Passing down parameters to a function
+
+def my_parameter_function(fname):
+  print(fname + " Refsnes")
+
+my_parameter_function("Emil")
